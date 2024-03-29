2c022645c3fdba6220f8d7ce72f4e78fbf032c86
Cris Vizcaino <cris.vizcaino@live.com>
2024-03-29 01:49:39 -0700

    made changes
    made changes to add user input

--- ZTM_AccountingSystem/ZTM_AccountingSystem/Program.cs
+++ ZTM_AccountingSystem/ZTM_AccountingSystem/Program.cs
@@ -6,20 +6,79 @@ using System.Security.Principal;
 using System;
 decimal transAmount = 0;
 Console.WriteLine("Welcome to the Accounting System 1.0!");
+
 Checking checking1 = new Checking(100, 0);
-Console.WriteLine($"The account with id {checking1.Id} has a balance of: {checking1.Balance}");
-
 Premium premium1 = new Premium(200, 0);
-Console.WriteLine($"The account with id {premium1.Id} has a balance of: {premium1.Balance}");
+string ans = "y";
 
-checking1 = new Checking(100, 2000);
+do
+{
+    Console.WriteLine("Select a number : Do you want to make a\n 1. Deposit\n 2. Transfer\n 3. Check balance");
+    char userSelection = Convert.ToChar(Console.ReadLine());
+    switch (userSelection)
+    {
+        case '1':
+            Console.WriteLine("Deposit");
+            Console.WriteLine("Choose an Account you want to make a Deposit to:\n 1. Checking\n 2. Premium");
+            char userSel = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("How much do you want to deposit");
+            decimal deposit = Convert.ToDecimal(Console.ReadLine());
+            if (userSel == '1')
+            {
+                checking1 = new Checking(100, deposit);
+
+            }
+            else if (userSel == '2')
+            {
+                premium1 = new Premium(200, deposit);
+            }
+
+            break;
+        case '2':
+            Console.WriteLine("transfer");
+            TransferMoney();
+            break;
+        case '3':
+            Console.WriteLine("Check Balance");
+            CheckBalance();
+            break;
+        default:
+            Console.WriteLine("Thank You");
+            break;
+    }
+    Console.WriteLine("Do you want to continue (y) or end (n)");
+    ans = Console.ReadLine().ToLower();
+
+}
+while (ans == "y");
+
+void TransferMoney()
+{
+    Console.WriteLine("Transfer from\n 1. Checking\n 2. Premium");
+    string userAns = Console.ReadLine();
+    if (userAns == "1")
+    {
+        Console.WriteLine("You are transfering from Checking to Premium");
+        Console.WriteLine("How much do you want to transfer? ");
+        transAmount = Convert.ToDecimal(Console.ReadLine());
+        checking1.Balance = checking1.Balance - transAmount;
+        premium1.Balance = premium1.Balance + transAmount;
+    }
+    else 
+    { 
+        Console.WriteLine("You are transfering from Premium to checking");
+        Console.WriteLine("How much do you want to transfer? ");
+        transAmount = Convert.ToDecimal(Console.ReadLine());
+        checking1.Balance = checking1.Balance + transAmount;
+        premium1.Balance = premium1.Balance - transAmount;
+    }
+}
+
 Console.WriteLine($"New balance for Account {checking1.Id} is: {checking1.Balance}");
-premium1 = new Premium(200, 6000);
+
 Console.WriteLine($"New balance for Account {premium1.Id} is: {premium1.Balance}");
 
-transAmount =Convert.ToDecimal(Console.ReadLine());
-checking1.Balance = checking1.Balance + transAmount;
-premium1.Balance = premium1.Balance - transAmount;
+
 
 
 
@@ -41,6 +100,12 @@ static void InterestRate(int id, decimal balance)
     }
     
 
+}
+
+void CheckBalance()
+{
+    Console.WriteLine($"The account with id {checking1.Id} has a balance of: {checking1.Balance}");
+    Console.WriteLine($"The account with id {premium1.Id} has a balance of: {premium1.Balance}");
 }
 
 public class Checking
